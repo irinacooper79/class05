ecf2d20b5f9afeed7addbf77ceb72e020761e5d0
Дом <ira-rostov80@mail.ru>
2022-10-05 15:38:14 +0300

    первый коммит класс-2

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,19 @@
-public class Author {
+public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        String name;
-        String surname;
-public Author(String name, String surname) {
-            this.name = name;
-            this.surname = surname;
-        }
-        public String getName (String name){
-
-            return name;
-        }
-        public String getSurname (String surname){
-
-            return surname;
-        }
-
-        public String fullName () {
-
-            return name + " " + surname;
-        }
-    }
-
-    public class Book {
-        String bookName;
-        Author author;
-        int published;
+        Author author1 = new Author("Виктор", "Пелевин");
+        Book book1 = new Book("Generation П", author1, 1999);
+        Author author2 = new Author("Виктор", "Пелевин");
+        Book book2 = new Book("Чапаев и Пустота", author2, 1996);
 
 
-        public Book(String bookName, Author author, int published) {
-            this.bookName = bookName;
-            this.author = author;
-            this.published = published;
-        }
+        System.out.println(book1.getBookName());
+        System.out.println(book1.getAuthor().fullName());
+        System.out.println(book1.getPublished());
 
-        public String getBookName() {
-            return bookName;
-        }
+        book1.setPublished(2001);
+        System.out.println(book1.getPublished());
 
-        public Author getAuthor() {
-            return author;
-        }
-
-        public int getPublished() {
-            return published;
-        }
-
-        public void setPublished(int published) {
-            this.published = published;
-        }
-    }
-
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello world!");
-
-            Author author1 = new Author("Виктор", "Пелевин");
-            Book book1 = new Book("Generation П", author1, 1999);
-            Author author2 = new Author("Виктор", "Пелевин");
-            Book book2 = new Book("Чапаев и Пустота", author2, 1996);
-
-
-            System.out.println(book1.getBookName());
-            System.out.println(book1.getAuthor().fullName());
-            System.out.println(book1.getPublished());
-
-            book1.setPublished(2001);
-            System.out.println(book1.getPublished());
-        }
     }
 }
-
